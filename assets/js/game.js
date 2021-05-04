e13a3b0c6d2ee36c209e7ced6864fee5964205ae
zoomzoom6 <tonyhuang107@live.ca>
2021-05-04 09:37:16 -0400

    add multiple rounds notification

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,10 +65,15 @@ var fight = function (enemyName) {
         }
     }
 };
-
-for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+if (playerHealth > 0) {
+    for (var i = 0; i < enemyNames.length; i++) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+} else {
+    window.alert("You have lost your roboot in battle! Game Over!");
+    break;
 }
 //fight();
